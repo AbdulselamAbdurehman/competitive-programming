8cecb7fea741cc717104f015fc3305ad9038286a
AbdulselamAbdurehman <119781939+AbdulselamAbdurehman@users.noreply.github.com>
2024-02-11 20:51:35 +0300

    Create binaryLinkedList.py
    time: O(n)
    space: O(1)
    I remember this question from final exam for DSA course.

---
+++ binaryLinkedList.py
@@ -0,0 +1,12 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:
+        decimal = 0
+        while head:
+            decimal = decimal*2 + head.val
+            head = head.next
+        return decimal
