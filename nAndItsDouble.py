060bb60208ecbc979beb87f8949c0a1b96030e99
AbdulselamAbdurehman <119781939+AbdulselamAbdurehman@users.noreply.github.com>
2024-03-14 11:43:13 +0300

    Create nAndItsDouble.py
    time: O(n)
    space: O(n)

---
+++ nAndItsDouble.py
@@ -0,0 +1,8 @@
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        seen = set()
+        for num in arr: 
+            if num/2 in seen or 2*num in seen:
+                return True
+            seen.add(num)
+        return False
