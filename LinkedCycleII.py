b4c38cc2ac840a4160b0ebe8254a26d48258c190
AbdulselamAbdurehman <119781939+AbdulselamAbdurehman@users.noreply.github.com>
2024-04-28 08:19:50 +0300

    Create LinkedCycleII.py
    time: O(n)
    space: O(1)

---
+++ LinkedCycleII.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if fast == slow:
+                while head != slow:
+                    head = head.next
+                    slow = slow.next
+                return head
+        return None
