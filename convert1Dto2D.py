1d627142e47d8be22623838e648d54f3756ea820
AbdulselamAbdurehman <119781939+AbdulselamAbdurehman@users.noreply.github.com>
2024-09-01 21:04:09 +0300

    Create convert1Dto2D.py
    time: O(m*n)
    space: O(m*n)
    where m and n are the orders of the new 2D array.

---
+++ convert1Dto2D.py
@@ -0,0 +1,5 @@
+class Solution:
+    def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
+        if m*n != len(original):
+            return []
+        return [[original[i*n+j] for j in range(n)] for i in range(m)]
